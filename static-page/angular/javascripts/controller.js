9e4093ff8c04a0de3ccab8980fa514e47e2344e1
liangjun.zhong <zhongliangjun1@gmail.com>
2014-09-02 09:57:43 +0800

    angular starting

---
+++ static-page/angular/javascripts/controller.js
@@ -0,0 +1,13 @@
+
+
+var myCtrl = function($scope) {
+
+	$scope.persons = [
+		{ 'name':'tom', 'sex':'male' },
+		{ 'name':'jack', 'sex':'male' },
+		{ 'name':'helen', 'sex':'male' },
+		{ 'name':'jessey', 'sex':'male' },
+		{ 'name':'kitty', 'sex':'female' }
+	];
+
+}
