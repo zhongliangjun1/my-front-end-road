3b6eebdbba6ff5428794f4c28adab8e095c7d9f5
liangjun.zhong <zhongliangjun1@gmail.com>
2014-09-03 23:11:05 +0800

    try ng-click ng-model ng-change

--- static-page/angular/javascripts/controller.js
+++ static-page/angular/javascripts/controller.js
@@ -2,12 +2,55 @@
 
 var myCtrl = function($scope) {
 
-	$scope.persons = [
-		{ 'name':'tom', 'sex':'male' },
-		{ 'name':'jack', 'sex':'male' },
-		{ 'name':'helen', 'sex':'male' },
-		{ 'name':'jessey', 'sex':'male' },
-		{ 'name':'kitty', 'sex':'female' }
+	var personsRepo = [
+		{ 'name':'tom', 'sex':'male', 'capacity':'abled' },
+		{ 'name':'jack', 'sex':'male', 'capacity':'disabled' },
+		{ 'name':'helen', 'sex':'male', 'capacity':'disabled' },
+		{ 'name':'jessey', 'sex':'male', 'capacity':'abled' },
+		{ 'name':'kitty', 'sex':'female', 'capacity':'abled' }
 	];
+
+	$scope.persons = personsRepo;
+
+	$scope.candidate = '';
+	$scope.addPerson = function() {
+		if ( $scope.candidate ) {
+			var newPerson = {
+				'name':$scope.candidate,
+				'sex':'male'
+			}
+			$scope.personsRepo.push(newPerson);
+			$scope.persons = personsRepo;
+			$scope.candidate = '';
+		};
+	}
+
+	$scope.query = function(condition) {
+		if ( condition ) {
+			var retainPeople = [];
+			for (var i = 0; i < $scope.persons.length; i++) {
+				var person = $scope.persons[i];	
+				if ( person.name.indexOf(condition)!=-1 ) {
+					retainPeople.push(person);
+				};
+			};
+			$scope.persons = retainPeople;
+		} else {
+			$scope.persons = personsRepo;
+		}
+	}
+
+	$scope.removeDisabledPeople = function() {
+		var abledPeople = [];
+		for (var i = 0; i < personsRepo.length; i++) {
+			var person = personsRepo[i];			
+			if ( 'abled' == person.capacity) {
+				abledPeople.push(person);
+			};
+		};
+		personsRepo = abledPeople;
+		$scope.persons = personsRepo;
+	}
+
 
 }
